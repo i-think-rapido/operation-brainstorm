40f95b6be9500def09f3274f308be59e136ac24d
Romeo Disca <romeo.disca@gmail.com>
2025-06-07 18:19:08 +0200

    fixing voxel color crate

--- voxel-color/src/lib.rs
+++ voxel-color/src/lib.rs
@@ -1,8 +1,11 @@
-pub use voxel_memory::VoxelColors;
+
+mod voxel_color;
+
+pub use voxel_color::*;
 
 #[no_mangle] 
 #[allow(improper_ctypes_definitions)]
-pub extern "C" fn new_voxel_color(x: usize, y: usize, z: usize) -> VoxelColors {
+pub extern "C" fn voxel_color(x: usize, y: usize, z: usize) -> VoxelColors {
     VoxelColors::new(x, y, z)
 }
 #[no_mangle] 
@@ -10,20 +13,20 @@ pub extern "C" fn voxel_color_capacity(voxel: &VoxelColors) -> usize {
     voxel.capacity()
 }
 #[no_mangle] 
-pub extern "C" fn voxel_color_r(voxel: &VoxelColors, index: usize) -> usize {
-    voxel.get_color(index, &RGBA::R).unwrap_or(0) as usize
+pub extern "C" fn voxel_color_r(voxel: &VoxelColors, index: usize) -> u8 {
+    voxel.get_color(index, &RGBA::R).unwrap_or(0) as u8
 }
 #[no_mangle] 
-pub extern "C" fn voxel_color_g(voxel: &VoxelColors, index: usize) -> usize {
-    voxel.get_color(index, &RGBA::G).unwrap_or(0) as usize
+pub extern "C" fn voxel_color_g(voxel: &VoxelColors, index: usize) -> u8 {
+    voxel.get_color(index, &RGBA::G).unwrap_or(0) as u8
 }
 #[no_mangle] 
-pub extern "C" fn voxel_color_b(voxel: &VoxelColors, index: usize) -> usize {
-    voxel.get_color(index, &RGBA::B).unwrap_or(0) as usize
+pub extern "C" fn voxel_color_b(voxel: &VoxelColors, index: usize) -> u8 {
+    voxel.get_color(index, &RGBA::B).unwrap_or(0) as u8
 }
 #[no_mangle] 
-pub extern "C" fn voxel_color_a(voxel: &VoxelColors, index: usize) -> usize {
-    voxel.get_color(index, &RGBA::A).unwrap_or(0) as usize
+pub extern "C" fn voxel_color_a(voxel: &VoxelColors, index: usize) -> u8 {
+    voxel.get_color(index, &RGBA::A).unwrap_or(0) as u8
 }
 #[no_mangle] 
 pub extern "C" fn set_voxel_color_r(voxel: &mut VoxelColors, index: usize, value: u8) {
@@ -43,145 +46,5 @@ pub extern "C" fn set_voxel_color_a(voxel: &mut VoxelColors, index: usize, value
 }
 
 
-#[derive(Debug, Clone, Copy, PartialEq)]
-#[repr(C)]
-pub enum RGBA { R, G, B, A, }
-impl From<&RGBA> for u8 {
-    fn from(v: &RGBA) -> u8 {
-        match v {
-            RGBA::R => 0,
-            RGBA::G => 1,
-            RGBA::B => 2,
-            RGBA::A => 3,
-        }
-    }
-}
 
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_voxel() {
-        let arr = [RGBA::R, RGBA::G, RGBA::B, RGBA::A];
-
-        let mut voxel_color = VoxelColors::new(3, 2, 1);
-
-        for ref rgba in arr {
-            assert_eq!(voxel_color.get_color(0, rgba), Some(0));
-            assert_eq!(voxel_color.get_color(1, rgba), Some(0));
-            assert_eq!(voxel_color.get_color(2, rgba), Some(0));
-            assert_eq!(voxel_color.get_color(3, rgba), Some(0));
-            assert_eq!(voxel_color.get_color(4, rgba), Some(0));
-            assert_eq!(voxel_color.get_color(5, rgba), Some(0));
-            assert_eq!(voxel_color.get_color(6, rgba), None);
-        }
-
-        for ref rgba in arr {
-            voxel_color.set_color(0, rgba, 13);
-        }
-        for ref rgba in arr {
-            assert_eq!(voxel_color.get_color(0, rgba), Some(13));
-        }
-
-        assert_eq!(voxel_color.capacity(), 6);
-    }
-}
-
-mod voxel_memory {
-    use std::alloc::{alloc, dealloc, Layout};
-    use getset::Getters;
-
-    use crate::RGBA;
-
-    #[derive(Getters, Debug)]
-    #[repr(C)]
-    pub struct VoxelColors {
-        #[getset(get)]
-        pub dim_x: usize,
-        #[getset(get)]
-        pub dim_y: usize,
-        #[getset(get)]
-        pub dim_z: usize,
-        
-        colors: Values,
-    }
-
-    impl VoxelColors {
-        pub fn new(dim_x: usize, dim_y: usize, dim_z: usize) -> Self {
-            VoxelColors {
-                dim_x,
-                dim_y,
-                dim_z,
-
-                colors: Values::new(dim_x as usize * dim_y * dim_z * 4), // 4 for RGBA
-            }
-        }
-
-        #[inline(always)]
-        pub fn capacity(&self) -> usize {
-            self.dim_x * self.dim_y * self.dim_z
-        }
-
-        pub fn get_color(&self, index: usize, rgba: &RGBA) -> Option<u8> {
-            self.colors.get(self::index(index, rgba))
-        }
-
-        pub fn set_color(&mut self, index: usize, rgba: &RGBA, value: u8) {
-            self.colors.set(self::index(index, rgba), value);
-        }
-
-        pub fn idx(&self, x: usize, y: usize, z: usize) -> Option<usize> {
-            if x >= self.dim_x || y >= self.dim_y || z >= self.dim_z { return None }
-            Some(x + y * self.dim_x + z * self.dim_x * self.dim_y)
-        }
-
-    }
-
-    #[inline(always)]
-    fn index(index: usize, rgba: &RGBA) -> usize {
-        index * 4 + u8::from(rgba) as usize
-    }
-
-    #[derive(Debug)]
-    #[repr(C)]
-    struct Values {
-        ptr: *mut u8,
-        capacity: usize,
-    }
-
-    impl Values {
-        pub fn new(capacity: usize) -> Self {
-            unsafe {
-                let layout = Layout::array::<u8>(capacity).expect("Failed to create layout for Values");
-                let ptr = alloc(layout);
-                ptr.write_bytes(0, capacity); // Initialize memory to zero
-                Values { ptr, capacity }
-            }
-        }
-
-        pub fn get(&self, index: usize) -> Option<u8> {
-            if index >= self.capacity {
-                return None;
-            }
-            unsafe { Some(*self.ptr.add(index)) }
-        }
-        pub fn set(&mut self, index: usize, value: u8) {
-            if index < self.capacity {
-                unsafe {
-                    *self.ptr.add(index) = value;
-                }
-          }
-        }
-    }
-
-    impl Drop for Values {
-        fn drop(&mut self) {
-            unsafe {
-                let layout = Layout::array::<u8>(self.capacity).expect("Failed to create layout for Values");
-                dealloc(self.ptr, layout);
-            }
-        }
-    }
-}
---
+++ voxel-color/src/voxel_color.rs
@@ -0,0 +1,145 @@
+use getset::Getters;
+
+
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+#[repr(C)]
+pub enum RGBA { R, G, B, A, }
+impl From<&RGBA> for u8 {
+    fn from(v: &RGBA) -> u8 {
+        match v {
+            RGBA::R => 0,
+            RGBA::G => 1,
+            RGBA::B => 2,
+            RGBA::A => 3,
+        }
+    }
+}
+
+#[derive(Getters, Debug)]
+#[repr(C)]
+pub struct VoxelColors {
+    #[getset(get)]
+    pub dim_x: usize,
+    #[getset(get)]
+    pub dim_y: usize,
+    #[getset(get)]
+    pub dim_z: usize,
+    
+    colors: memory::Values,
+}
+
+impl VoxelColors {
+    pub fn new(dim_x: usize, dim_y: usize, dim_z: usize) -> Self {
+        VoxelColors {
+            dim_x,
+            dim_y,
+            dim_z,
+
+            colors: memory::Values::new(dim_x * dim_y * dim_z * 4), // 4 for RGBA
+        }
+    }
+
+    #[inline(always)]
+    pub fn capacity(&self) -> usize {
+        self.dim_x * self.dim_y * self.dim_z
+    }
+
+    pub fn get_color(&self, index: usize, rgba: &RGBA) -> Option<u8> {
+        self.colors.get(memory::index(index, rgba))
+    }
+
+    pub fn set_color(&mut self, index: usize, rgba: &RGBA, value: u8) {
+        self.colors.set(memory::index(index, rgba), value);
+    }
+
+    pub fn idx(&self, x: usize, y: usize, z: usize) -> Option<usize> {
+        if x >= self.dim_x || y >= self.dim_y || z >= self.dim_z { return None }
+        Some(x + y * self.dim_x + z * self.dim_x * self.dim_y)
+    }
+
+}
+
+mod memory {
+
+    use super::RGBA;
+
+    use std::alloc::{alloc, dealloc, Layout};
+
+    #[inline(always)]
+    pub fn index(index: usize, rgba: &RGBA) -> usize {
+        index * 4 + u8::from(rgba) as usize
+    }
+
+    #[derive(Debug)]
+    #[repr(C)]
+    pub struct Values {
+        ptr: *mut u8,
+        capacity: usize,
+    }
+
+    impl Values {
+        pub fn new(capacity: usize) -> Self {
+            unsafe {
+                let layout = Layout::array::<u8>(capacity).expect("Failed to create layout for Values");
+                let ptr = alloc(layout);
+                ptr.write_bytes(0, capacity); // Initialize memory to zero
+                Values { ptr, capacity }
+            }
+        }
+
+        pub fn get(&self, index: usize) -> Option<u8> {
+            if index >= self.capacity {
+                return None;
+            }
+            unsafe { Some(*self.ptr.add(index)) }
+        }
+        pub fn set(&mut self, index: usize, value: u8) {
+            if index < self.capacity {
+                unsafe {
+                    *self.ptr.add(index) = value;
+                }
+          }
+        }
+    }
+
+    impl Drop for Values {
+        fn drop(&mut self) {
+            unsafe {
+                let layout = Layout::array::<u8>(self.capacity).expect("Failed to create layout for Values");
+                dealloc(self.ptr, layout);
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_voxel() {
+        let arr = [RGBA::R, RGBA::G, RGBA::B, RGBA::A];
+
+        let mut voxel_color = VoxelColors::new(3, 2, 1);
+
+        for ref rgba in arr {
+            assert_eq!(voxel_color.get_color(0, rgba), Some(0));
+            assert_eq!(voxel_color.get_color(1, rgba), Some(0));
+            assert_eq!(voxel_color.get_color(2, rgba), Some(0));
+            assert_eq!(voxel_color.get_color(3, rgba), Some(0));
+            assert_eq!(voxel_color.get_color(4, rgba), Some(0));
+            assert_eq!(voxel_color.get_color(5, rgba), Some(0));
+            assert_eq!(voxel_color.get_color(6, rgba), None);
+        }
+
+        for ref rgba in arr {
+            voxel_color.set_color(0, rgba, 13);
+        }
+        for ref rgba in arr {
+            assert_eq!(voxel_color.get_color(0, rgba), Some(13));
+        }
+
+        assert_eq!(voxel_color.capacity(), 6);
+    }
+}
